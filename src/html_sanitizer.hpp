e3b205c30561b0309ec933d71b5d1384ef729024
MetroWind <chris.corsair@gmail.com>
2026-06-01 11:42:16 -0700

    Reset the project

--- src/html_sanitizer.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-class HtmlSanitizer
-{
-public:
-    static std::string sanitize(const std::string& input);
-};
--- src/http_utils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-namespace http_utils
-{
-std::string getHttpDate();
-bool checkDateSkew(const std::string& date_str, int max_skew_seconds = 30);
-} // namespace http_utils
